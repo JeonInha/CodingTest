83761ede232209ae0e94e5adffcd8b28c4062b3f
전인하 <104603885+JeonInha@users.noreply.github.com>
2022-05-24 17:04:43 +0900

    Add files via upload

---
+++ BJ10824.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BJ10824 {
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		
+	    String a, b, c, d;
+		a=sc.next();
+		b=sc.next();
+		c=sc.next();
+		d=sc.next();
+		
+		String ab=a+b;
+		String cd=c+d;
+
+        System.out.println(Long.parseLong(ab)+Long.parseLong(cd));
+	}
+}
